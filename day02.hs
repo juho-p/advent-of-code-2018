eab8a3573ea13857665dc5f1288e815df02ed5ff
Juho Peltonen <juhopel@gmail.com>
2018-12-02 23:33:53 +0200

    Possibly more correct day2

--- day02.hs
+++ day02.hs
@@ -2,35 +2,37 @@
 
 import           Text.Parsec
 import           Text.Parsec.String
-import qualified Data.Map.Strict               as Map
 import           Data.Maybe
-import           Data.Set                hiding ( splitAt )
 import           Control.Monad
-import           Data.List                      ( find
-                                                , splitAt
-                                                )
-import           Debug.Trace
+import           Data.List
+import qualified Data.Map.Strict               as Map
+import qualified Data.Set                      as S
 
 parser :: Parser [String]
 parser = many identifier <* eof where identifier = many1 letter <* spaces
 
+part1 :: [String] -> Int
 part1 ids = appearences 2 * appearences 3
   where
+    appearences :: Int -> Int
     appearences n = length $ mfilter (appear n) $ fmap histogram ids
     appear x m = elem x $ Map.elems m
     histogram = Map.fromListWith (+) . fmap (, 1)
 
-part2 = solve empty
+part2 :: [String] -> String
+part2 ids = head solutions
   where
-    solve acc (x : xs) = case match x acc of
-        (_, Just a ) -> a
-        (s, Nothing) -> solve s xs
-    match x s =
-        let x' = removeOne x in (s `union` fromList x', find (`member` s) x')
+    solutions = (maybeToList . findNonUnique S.empty) =<< idLists
+    idLists   = transpose $ removeOne <$> ids
     removeOne :: String -> [String]
     removeOne (x : xs) = xs : ((x :) <$> removeOne xs)
     removeOne ""       = []
+    findNonUnique :: S.Set String -> [String] -> Maybe String
+    findNonUnique acc (x : xs) | S.member x acc = Just x
+                               | otherwise = findNonUnique (S.insert x acc) xs
+    findNonUnique _ [] = Nothing
 
+main :: IO ()
 main = do
     values <- parseFromFile parser "input/day02.txt"
     print $ part1 <$> values
